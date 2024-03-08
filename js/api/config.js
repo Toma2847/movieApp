6e80856b618ccd35f8b126cf329a441075701aef
Francesco Tomaciello <francesco.tomaciello@gmail.com>
2024-03-08 11:11:28 +0100

    aggiunta forEach

--- js/api/config.js
+++ js/api/config.js
@@ -1,10 +1,15 @@
+//ipmport del package che mi rende disponibili le variabili d'ambiente
+import dotenv from 'dotenv';
+
+//carica le variabili d'ambiente del file .env
+dotenv.config();
+
 export const baseUrl = "https://api.themoviedb.org/3/";
 
 export const OPTIONS = {
     headers: {
         "Content-Type": 'application/json',
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTg2OTExMjc0MWIxODc4ZGM0ODJlNTg0YjY5Y2JjMyIsInN1YiI6IjY1ZTg2YjAxYTFhOWJhMDE0OGJkZTBkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.97P7I4KG6HXcC6IVuTApkKIzhef33WT0kjVH8GENgOM"
-
+        Authorization: `Bearer ${process.env.API_KEY}`
     },
 };
 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,27 @@
 import {getTrendingMovies, getTrending, getTrendingActor} from "./api/trendingGetters.js";
 
-getTrending();
+
+
+/**da aggiungere:
+ * id
+ * poster_path
+ * title
+ * vote_average
+ * media_type
+ * release_date
+ */
+
+const trending = getTrending().then((data) => {
+    const trending = data.results
+    trending.forEach((element) => {
+        console.log('title' , element.title);
+        console.log('poster', element.poster_path);
+        console.log('id', element.id);
+        console.log('release date', element.release_date);
+        console.log('media type', element.media_type);
+        console.log('vote ', element.vote_average);
+        })
+});
+
 getTrendingMovies();
 getTrendingActor();
